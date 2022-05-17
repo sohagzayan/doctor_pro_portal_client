cb88135ad630861ddba8725266dac76a118562ab
sohagmia360 <gdsohag360@gmail.com>
2022-05-17 07:39:46 +0600

    add about us section and make some responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import heroOrnament from '../assets/images/heroOrnament.jpg';
-import Header from '../components/Header.js/Header';
-import Hero from '../components/Hero.js/Hero';
-import OurLocation from '../components/OurLocation/OurLocation';
-import OurServices from '../components/OurServices/OurServices';
+import React from "react";
+import heroOrnament from "../assets/images/heroOrnament.jpg";
+import AboutUs from "../components/AboutUs/AboutUs";
+import Header from "../components/Header.js/Header";
+import Hero from "../components/Hero.js/Hero";
+import OurLocation from "../components/OurLocation/OurLocation";
+import OurServices from "../components/OurServices/OurServices";
 const Home = () => {
-    return (
-        <div>
-            <div style={{
-                backgroundImage : `url(${heroOrnament})`,
-                backgroundRepeat : "no-repeat",
-                backgroundPosition : "center" ,
-                backgroundSize : 'cover'
-            }} className=''>
-                <Header />
-                <Hero />
-                <OurLocation />
-                <OurServices />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${heroOrnament})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+        className=""
+      >
+        <Header />
+        <Hero />
+      </div>
+      <OurLocation />
+      <OurServices />
+      <AboutUs />
+    </div>
+  );
 };
 
 export default Home;
